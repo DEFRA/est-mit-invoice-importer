700f0ba4e4a6d8881f8443b2efbcb5988ac68edc
Nick Blows <Nicholas.Blows@rpa.gov.uk>
2023-03-28 11:12:47 +0100

    moved blob service into seperate class

---
+++ EST.MIT.InvoiceImporter.Function/Services/BlobService.cs
@@ -0,0 +1,47 @@
+﻿using Azure.Core;
+using InvoiceImporter.Function.Models;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EST.MIT.InvoiceImporter.Function.Services
+{
+    public class BlobService : IBlobService
+    {
+        public async Task<Stream> ReadBLOBIntoStream(string importMsg, ILogger log, IBinder blobBinder)
+        {
+            Stream blobStream = null;
+            ImportRequest importRequest = null;
+
+            if (importMsg == null)
+            {
+                log.LogError("No import request received.");
+                return blobStream;
+            }
+
+            try
+            {
+                importRequest = JsonConvert.DeserializeObject<ImportRequest>(importMsg);
+            }
+            catch (JsonException ex)
+            {
+                log.LogError(ex, "Invalid import request received.");
+                return blobStream;
+            }
+
+            var blobAttr = new BlobAttribute($"invoices/import/{importRequest.FileName}", FileAccess.Read)
+            {
+                Connection = "StorageConnectionString"
+            };
+
+            return await blobBinder.BindAsync<Stream>(blobAttr);
+        }
+    }
+}
---
+++ EST.MIT.InvoiceImporter.Function/Services/IBlobService.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace EST.MIT.InvoiceImporter.Function.Services
+{
+    public interface IBlobService
+    {
+        Task<Stream> ReadBLOBIntoStream(string importMsg, ILogger log, IBinder blobBinder);
+    }
+}
