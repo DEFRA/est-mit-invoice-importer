e960ded2f68164c02461a641c9be024bf636207b
Nick Blows <Nicholas.Blows@rpa.gov.uk>
2023-04-03 16:44:30 +0100

    added coverage exclusion

--- EST.MIT.InvoiceImporter.Function/Startup.cs
+++ EST.MIT.InvoiceImporter.Function/Startup.cs
@@ -2,6 +2,7 @@ using EST.MIT.Importer.Function.Services;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 [assembly: FunctionsStartup(typeof(Startup.Function.Startup))]
 
@@ -9,6 +10,7 @@ namespace Startup.Function
 {
     public class Startup : FunctionsStartup
     {
+        [ExcludeFromCodeCoverage]
         public override void Configure(IFunctionsHostBuilder builder)
         {
             Console.WriteLine("Configuring service...");
