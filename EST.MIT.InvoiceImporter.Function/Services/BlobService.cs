c9d59fc75e25b3819790f1def743e8f4c0d15144
Nick Blows <Nicholas.Blows@rpa.gov.uk>
2023-04-04 11:05:18 +0100

    added write method

--- EST.MIT.InvoiceImporter.Function/Services/BlobService.cs
+++ EST.MIT.InvoiceImporter.Function/Services/BlobService.cs
@@ -1,4 +1,5 @@
-﻿using InvoiceImporter.Function.Models;
+﻿using Azure.Storage.Blobs;
+using InvoiceImporter.Function.Models;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
@@ -36,6 +37,41 @@ namespace EST.MIT.InvoiceImporter.Function.Services
             };
 
             return await blobBinder.BindAsync<Stream>(blobAttr);
+        }
+
+        //Write a stream to a blob
+        public async Task<bool> WriteStreamToBLOB(string blobName, ILogger log, IBinder blobBinder)
+        {
+            try
+            {
+                var blobAttr = new BlobAttribute($"invoices/import/archive/{blobName}", FileAccess.Write)
+                {
+                    Connection = "StorageConnectionString"
+                };
+                var blobBinding = await blobBinder.BindAsync<Stream>(blobAttr);
+                return true;
+            }
+            catch (JsonException ex) { log.LogError($"An error occured when creating an archive file: [{0}]", ex); }
+        }
+
+        public async Task<bool> MoveFileIntoArchive(string fileName, ILogger log, IBinder blobBinder)
+        {
+            //Copy a file matching the fileName to the archive folder in the blob storage
+            var sourceFile = new BlobAttribute($"invoices/import/{fileName}", FileAccess.Read)
+            {
+                Connection = "StorageConnectionString"
+            };
+
+            var destFile = new BlobAttribute($"invoices/archive/{fileName}", FileAccess.Write)
+            {
+                Connection = "StorageConnectionString"
+            };
+
+            var sourceBinding = await blobBinder.BindAsync<Stream>(sourceFile);
+
+            var destBinding = await blobBinder.BindAsync<Stream>(destFile);
+            
+        return true;
         }
     }
 }
